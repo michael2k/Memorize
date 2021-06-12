ff085d6e90f6d7a0889f692f056cfb78e58c88a9
michael2k <mskim2k@gmail.com>
2021-06-12 13:07:37 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Michael M. Kim on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
